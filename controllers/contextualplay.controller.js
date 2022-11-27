ba46839f3cc6a90d97020d23d0b3b7bbd4de93c3
Kabir Gupta <85513702+KabirGupta07@users.noreply.github.com>
2022-11-27 13:20:05 +0530

    Modified

--- controllers/contextualplay.controller.js
+++ controllers/contextualplay.controller.js
@@ -3,8 +3,9 @@ exports.postPlayData = async (req, res, next) =>{
     let contextual_id = req.params.contextual_id;
     let played_at = req.body.played_at;
     let device_id = req.body.device_id;
+    let phrase = req.body.phrase;
 
-    let contextualPlay = new ContextualPlay(device_id, contextual_id, played_at);
+    let contextualPlay = new ContextualPlay(device_id, contextual_id, played_at, phrase);
     console.log(contextualPlay);
     try{
         const [data, _] = await contextualPlay.save();
--- models/contextualplay.model.js
+++ models/contextualplay.model.js
@@ -1,10 +1,11 @@
 const db = require('../mysql/mysqlConnection');
 const database = "contextual_play";
 class ContextualPlay{
-    constructor(device_id, contextual_id, played_at){
+    constructor(device_id, contextual_id, played_at, phrase){
         this.device_id = device_id;
         this.contextual_id = contextual_id;
         this.played_at = played_at;
+        this.phrase = phrase;
     }
 
     save(){
@@ -13,12 +14,14 @@ class ContextualPlay{
         INSERT INTO ${database}(
             device_id,
             contextual_id,
-            played_at
+            played_at,
+            phrase  
         )
         values(
             ${this.device_id},
             ${this.contextual_id},
-            "${this.played_at}"
+            "${this.played_at}",
+            "${this.phrase}"
         )`;
         console.log(sql);
         return db.execute(sql);
@@ -33,5 +36,4 @@ class ContextualPlay{
         return db.execute(sql);
     }
 }
-
 module.exports = ContextualPlay;
